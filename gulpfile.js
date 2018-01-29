89892a64aa17e65bd51ee28fdae6ffd0df6bc53d
Igor <boogeyster@gmail.com>
2018-01-30 04:46:57 +0800

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+gulp.task('default',function(){
+  console.log('Hurrah - you created a task');
+
+})
+
+gulp.task('html',function(){
+  console.log('Imagine something useful being done to your HTML here.');
+
+})
