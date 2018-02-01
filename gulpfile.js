2a34bba8abb8d78617d8a3185a0f55b8506aa070
Igor <boogeyster@gmail.com>
2018-02-01 11:05:57 +0800

    browser-sync issue,rolled back to 2.12.8

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,46 @@
 var gulp = require('gulp'),
-    watch= require('gulp-watch'),
-    postcss=require('gulp-postcss'),
-    autoprefixer=require('autoprefixer'),
-    cssvars=require('postcss-simple-vars'),
-    nested=require('postcss-nested'),
-    cssImport=require('postcss-import');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
-
-gulp.task('default',function(){
-  console.log('Hurrah - you created a task');
-
-})
-
-gulp.task('html',function(){
-  console.log('Imagine something useful being done to your HTML here.');
-
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('styles',function(){
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
 
 });
 
-gulp.task('watch',function(){
-  watch('./app/index.html',function(){
-    gulp.start('html');
-  });
-
-  watch('./app/assets/styles/**/*.css',function(){
-    gulp.start('styles');
-
-  });
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
