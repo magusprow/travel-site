ae41ce30600c4e605506416ab2b2837eabeb3d49
Igor <boogeyster@gmail.com>
2018-02-17 23:31:07 +0800

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,8 +2,6 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-
-
 gulp.task('watch', function() {
 
   browserSync.init({
@@ -21,10 +19,17 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
-});
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 
+});
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
